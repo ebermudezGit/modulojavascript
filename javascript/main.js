e0757c17916c467f815cd3dad710d6ff637d94f6
Eli Bermudez <bermudez.86@live.com.mx>
2016-05-17 19:58:28 -0500

    First Commit

---
+++ javascript/main.js
@@ -0,0 +1,68 @@
+alert("Hola mundo")
+
+function Validar(){
+	confirm("Llamada de function");
+};
+
+function ValidarFor () {
+	 var numero = prompt("Ingresa un numero para Factorial");
+	 var factorial =1;
+	 for (var i = 1; i <= numero; i++) {
+	 	factorial=factorial*i;
+	 }
+
+	 alert("El factorial de "+ numero+" es: "+factorial);
+}
+
+
+function fncodigosinfuncion() {
+	var resultado=0;
+	var numero1 = 3;
+	var numero2 = 5;
+			
+	//	Se	suman	los	números	y	se	muestra	el	resultado
+	resultado = numero1 + numero2;
+	alert("El resultado es " + resultado);
+			
+	numero1 = 10;
+	numero2 = 7;
+			
+	//	Se	suman	los	números	y	se	muestra	el	resultado	
+	resultado = numero1 + numero2;
+	alert("El resultado es " + resultado);
+			
+	numero1 = 5;
+	numero2 = 8;
+			
+	//	Se	suman	los	números	y	se	muestra	el	resultado
+	resultado = numero1 + numero2;
+	alert("El resultado es " + resultado);
+}
+
+function fntestSuma(numero1,numero2) {
+	var resultado=0;
+	
+	resultado = numero1 + numero2;
+	alert("El resultado es " + resultado);
+}
+
+function fncodigoconfuncion() {
+	var numero1 = 3;
+	var numero2 = 5;
+			
+	//	Se	suman	los	números	y	se	muestra	el	resultado
+	fntestSuma(numero1,numero2);
+			
+	numero1 = 10;
+	numero2 = 7;
+			
+	//	Se	suman	los	números	y	se	muestra	el	resultado	
+	fntestSuma(numero1,numero2);
+			
+	numero1 = 5;
+	numero2 = 8;
+			
+	//	Se	suman	los	números	y	se	muestra	el	resultado
+	fntestSuma(numero1,numero2);
+	
+}
